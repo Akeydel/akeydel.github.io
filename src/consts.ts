6a8ce45e2f3f819e9f01f540e8a12d14decc8ac3
Akeydel <Akeydel@gmail.com>
2026-04-09 18:02:32 -0600

    init

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'AKeydel';
+export const SITE_DESCRIPTION = 'Akeydel - Personal Site';
+export const BG_COLOR = '#15153A'
